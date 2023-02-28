a09bdf672387ecf7fa3c092f7b12032940873875
gus2902 <gus2902@naver.com>
2023-02-28 17:10:56 +0900

    스터디

---
+++ Study Code/Winform_Study/Winform_Study/UserControl1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Winform_Study
+{
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+            this.Controls.Add(pBar);
+
+            Label lb_pBar = new Label();
+            lb_pBar.Parent = pBar;
+            lb_pBar.Text = "0%";
+            lb_pBar.AutoSize = true;
+            lb_pBar.Location = new Point(pBar.Width / 2, pBar.Height / 2);
+            lb_pBar.BackColor = Color.Transparent;
+            
+            label1.Parent = pBar;
+            label1.BackColor = Color.Transparent;
+            label1.Location = new Point(0, 0);
+
+            this.Controls.Add(lb_pBar);
+            this.Controls.SetChildIndex(lb_pBar, 0);
+
+        }
+
+        private void progressBar1_Paint(object sender, PaintEventArgs e)
+        {
+            // 진행 상황 계산
+            int percentage = (int)(((double)pBar.Value / (double)pBar.Maximum) * 100);
+
+            // 그리기 작업
+            using (Font font = new Font("Arial", 10))
+            {
+                string text = percentage.ToString() + "%";
+                SizeF textSize = e.Graphics.MeasureString(text, font);
+                PointF textPoint = new PointF(pBar.Width / 2 - textSize.Width / 2,
+                                              pBar.Height / 2 - textSize.Height / 2);
+
+                e.Graphics.DrawString(text, font, Brushes.Black, textPoint);
+            }
+        }
+    }
+}
---
+++ Study Code/Winform_Study/Winform_Study/fMain.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Winform_Study
+{
+    partial class fMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userControl11 = new Winform_Study.UserControl1();
+            this.textProgressBar1 = new Winform_Study.TextProgressBar();
+            this.SuspendLayout();
+            // 
+            // userControl11
+            // 
+            this.userControl11.Location = new System.Drawing.Point(0, 0);
+            this.userControl11.Name = "userControl11";
+            this.userControl11.Size = new System.Drawing.Size(813, 201);
+            this.userControl11.TabIndex = 0;
+            // 
+            // textProgressBar1
+            // 
+            this.textProgressBar1.CustomText = "";
+            this.textProgressBar1.Location = new System.Drawing.Point(323, 290);
+            this.textProgressBar1.Name = "textProgressBar1";
+            this.textProgressBar1.ProgressColor = System.Drawing.Color.LightGreen;
+            this.textProgressBar1.Size = new System.Drawing.Size(342, 46);
+            this.textProgressBar1.TabIndex = 1;
+            this.textProgressBar1.TextColor = System.Drawing.Color.Black;
+            this.textProgressBar1.TextFont = new System.Drawing.Font("Times New Roman", 11F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.textProgressBar1.VisualMode = Winform_Study.ProgressBarDisplayMode.Percentage;
+            // 
+            // fMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textProgressBar1);
+            this.Controls.Add(this.userControl11);
+            this.Name = "fMain";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private UserControl1 userControl11;
+        private TextProgressBar textProgressBar1;
+    }
+}
+
